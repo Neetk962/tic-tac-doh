53312e2d6d520e5567ddb4bd781de6a142c896c5
athenamw <wallisathena@gmail.com>
2023-10-07 22:46:26 -0700

    add button styling

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -94,7 +94,7 @@ const Game = () => {
             {winner && (
                 <>
                     <p>{winner} is the winner!</p>
-                    <button onClick={() => handlePlayAgain()}>Play Again</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => handlePlayAgain()}>Play Again</button>
                 </>
             )}
 
