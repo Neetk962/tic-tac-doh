47d626c0d08548059ba4524ebd863600146b372b
Jesus Santos <jo_nez@hotmail.com>
2023-10-11 22:07:41 -0700

    server and graphql running err on browser

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/tictacdoh",
-);
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+        console.log(err)
+    });
 
 module.exports = mongoose.connection;
