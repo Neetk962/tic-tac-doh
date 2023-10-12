baeaabaacb1f1c450d5d110c01035663faddb7b9
athenamw <wallisathena@gmail.com>
2023-10-11 20:52:43 -0700

    Revert "install socket"
    This reverts commit f78bd31600431f59b5e117e030c5d47d3e283d6f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import Game from "./components/game/Game";
-import io from "socket.io-client";
-const socket = io('http://localhost:4000');
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const PORT = 4000;
-const INDEX = "index.html";
-
-const app = express();
-app.use((_req, res) => res.sendFile(INDEX, { root: __dirname }));
-
-const server = app.listen(PORT, () => console.log("Listening"));
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
