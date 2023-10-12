f78bd31600431f59b5e117e030c5d47d3e283d6f
athenamw <wallisathena@gmail.com>
2023-10-11 20:52:43 -0700

    install socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import Game from "./components/game/Game";
+import io from "socket.io-client";
+const socket = io('http://localhost:4000');
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const PORT = 4000;
+const INDEX = "index.html";
+
+const app = express();
+app.use((_req, res) => res.sendFile(INDEX, { root: __dirname }));
+
+const server = app.listen(PORT, () => console.log("Listening"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
