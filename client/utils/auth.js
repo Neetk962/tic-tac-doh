52f306239e4041cde2e215b913b4e94cadde3f7b
MyComplex <jo_nez@hotmail.com>
2023-10-13 17:06:44 -0700

    WIPPING auth

--- client/utils/auth.js
+++ client/utils/auth.js
@@ -8,10 +8,43 @@ class AuthService {
 
     loggedIn() {
         const token = this.getToken();
+
         if (token && !this.isTokenExpired(token)) {
-            return false;
-        } else {
             return true;
+        } else {
+            return false;
         }
     }
+
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    }
+
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
+
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+
+        window.location.assign('/');
+    }
 }
+
+export default new AuthService();
