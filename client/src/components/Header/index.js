0f0c475fde1113aa316df10a1a2639356d28d15b
Jesus Santos <jo_nez@hotmail.com>
2023-10-15 11:59:38 -0700

    moved title and image to header el

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
+import dohimg from "../../assets/doh.png"
 
 const Header = () => {
     const logout = (event) => {
@@ -11,11 +12,25 @@ const Header = () => {
         <header>
             <div>
                 <div>
-                    <Link to="/">Home</Link>
-                    <Link to="/game">Play</Link>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
-                    <button onClick={logout}>Logout</button>
+                    <h1 className='simpsonfont text-amber-400'>Tic Tac <img id="dohimg" src={dohimg} alt="doh" /></h1>
+                    
+                </div>
+                <div>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <span>Welcome, {Auth.getProfile().data.username}!</span>
+                            <Link to="/game">Play</Link>
+                            <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/">Home</Link>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </>
+                    )}
                 </div>
             </div>
         </header>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
                 <div>
                     <div>
                         <h2>
-                            Welcome to the home page {userData.username}!
+                            This is the home page!
                         </h2>
                     </div>
                 </div>
