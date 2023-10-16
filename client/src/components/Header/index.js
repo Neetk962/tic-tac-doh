a4fde901734b4c32e1a6c3d9e1b088351ae96afb
Ravneet Kaur <ravneet.kaur9629@gmail.com>
2023-10-16 08:34:42 -0700

    updated sound

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,17 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 import dohimg from "../../assets/images/doh.png"
+import soundFile from "../../assets/sounds/boring.mp3";
 
+// function playSound() {
+//     const audio = new Audio(soundFile);
+//     audio.play();
+// }
 const Header = () => {
     const logout = (event) => {
         event.preventDefault();
         Auth.logout();
+        const audio = new Audio(soundFile);
+    audio.play();
     };
     return (
         <header>
             <div>
-                <div>
-                    <h1 className='simpsonfont text-amber-400'>Tic Tac <img id="dohimg" src={dohimg} alt="doh" /></h1>
+                <div className="flex-row">
+                    <h1 className='simpsonfont text-amber-400 title'>Tic Tac </h1>
+                    <img id="dohimg" src={dohimg} alt="doh" />
                     
                 </div>
                 <div>
@@ -20,7 +28,8 @@ const Header = () => {
                         <>
                             <span>Welcome, {Auth.getProfile().data.username}!</span>
                             <Link to="/game">Play</Link>
-                            <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                            <button 
+                            className="btn btn-lg btn-light m-2" onClick={logout}>
                                 Logout
                             </button>
                         </>
